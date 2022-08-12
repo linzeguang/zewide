6822867a14732da2791148bab3636f81c20f56b1
linzeguang <15817289730@163.com>
2022-08-12 16:05:40 +0800

    clear dist

--- dist/esm/components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { jsx as _jsx } from 'react/jsx-runtime';
-export var Button = function Button() {
-  return /*#__PURE__*/ _jsx('button', {
-    children: 'Button',
-  });
-};
