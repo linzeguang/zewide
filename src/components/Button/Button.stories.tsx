eed2007dbfe0d02432e7750bda87cb6f36f6195f
linzeguang <15817289730@163.com>
2022-08-14 15:41:59 +0800

    使用storybook做文档

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  text: 'Button',
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,15 @@
 /*
  * @Author: linzeguang
  * @Date: 2022-08-13 02:30:44
- * @LastEditTime: 2022-08-13 03:09:06
+ * @LastEditTime: 2022-08-14 15:30:52
  * @LastEditors: linzeguang
- * @Description:
+ * @Description: Button 组件
  */
-import React from "react";
-import styled from "styled-components";
-import { ButtonProps } from "./types";
+
+import React from 'react';
+import styled from 'styled-components';
+
+import type { ButtonProps } from './types';
 
 const StyledButton = styled.button<ButtonProps>`
   border: 0;
@@ -19,25 +21,25 @@ const StyledButton = styled.button<ButtonProps>`
   border-radius: 3px;
   display: inline-block;
   padding: ${(props) =>
-    props.size === "small"
-      ? "7px 25px 8px"
-      : props.size === "medium"
-      ? "9px 30px 11px"
-      : "14px 30px 16px"};
-  color: ${(props) => (props.primary ? "#1b116e" : "#ffffff")};
-  background-color: ${(props) => (props.primary ? "#6bedb5" : "#1b116e")};
+    props.size === 'small'
+      ? '7px 25px 8px'
+      : props.size === 'medium'
+      ? '9px 30px 11px'
+      : '14px 30px 16px'};
+  color: ${(props) => (props.primary ? '#1b116e' : '#ffffff')};
+  background-color: ${(props) => (props.primary ? '#6bedb5' : '#1b116e')};
   opacity: ${(props) => (props.disabled ? 0.5 : 1)};
   &:hover {
-    background-color: ${(props) => (props.primary ? "#55bd90" : "#6bedb5")};
+    background-color: ${(props) => (props.primary ? '#55bd90' : '#6bedb5')};
   }
   &:active {
-    border: solid 2px #1b116e;
     padding: ${(props) =>
-      props.size === "small"
-        ? "5px 23px 6px"
-        : props.size === "medium"
-        ? "7px 28px 9px"
-        : "12px 28px 14px"};
+      props.size === 'small'
+        ? '5px 23px 6px'
+        : props.size === 'medium'
+        ? '7px 28px 9px'
+        : '12px 28px 14px'};
+    border: solid 2px #1b116e;
   }
 `;
 
