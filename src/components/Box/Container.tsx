a5200d9bb2602cf49cfc23f464e48294d2f7316e
linzeguang <15817289730@163.com>
2022-08-29 14:17:06 +0800

    update

--- src/components/Box/Container.tsx
+++ src/components/Box/Container.tsx
@@ -4,6 +4,6 @@ import { Box } from './Box';
 
 export const Container = styled(Box)`
   width: 100%;
-  max-width: ${({ theme }) => theme.siteWidth};
+  max-width: ${({ theme }) => theme.siteWidth}px;
   margin: 0 auto;
 `;
