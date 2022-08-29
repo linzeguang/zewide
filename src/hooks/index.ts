d7c5ff93cc1927436d49a76f0c8edb656dba7465
linzeguang <15817289730@163.com>
2022-08-29 14:19:31 +0800

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,6 @@
 export { default as useAuth } from './useAuth';
 export { default as useEagerlyConnect } from './useEagerlyConnect';
+export * from './useIsomorphicEffect';
+export { default as useMatchBreakpoints } from './useMatchBreakpoints';
 export { default as useMetamask } from './useMetamask';
 export * from './useStorage';
