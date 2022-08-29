9293cefd5ca37ff344160afc057a44df081e6f62
linzeguang <15817289730@163.com>
2022-08-29 11:17:55 +0800

    Update ModalContext.tsx

--- src/widgets/Modal/ModalContext.tsx
+++ src/widgets/Modal/ModalContext.tsx
@@ -82,7 +82,7 @@ const ModalProvider: React.FC<PropsWithChildren> = ({ children }) => {
               <BodyLock />
               <Overlay onClick={handleOverlayDismiss} />
               {React.isValidElement(modalNode) &&
-                React.cloneElement(modalNode, {
+                React.cloneElement<{ onDismiss: ModalsContext['onDismiss'] }>(modalNode, {
                   onDismiss: handleDismiss,
                 })}
             </ModalWrapper>
